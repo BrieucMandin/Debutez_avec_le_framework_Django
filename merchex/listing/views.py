c6487647d3fa63ec13d2393bdb32d845809aa23f
Brieuc Mandin <brieucmandin@macbook.local>
2025-04-10 10:56:33 +0200

    Partie 1 Démarage djago jusqu'à band

---
+++ merchex/listing/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from listing.models import Band
+
+# Create your views here.
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+        </ul>
+""")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
