3d41ffc53bdf752b5765b9d949354e577ec744cc
Brieuc Mandin <brieucmandin@macbook.local>
2025-04-10 15:39:45 +0200

    Django interface administrateur

--- merchex/listing/admin.py
+++ merchex/listing/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from listing.models import Band, Listings # Importer le modèle Band et Listings
 # Register your models here.
+
+
+class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('name', 'year_formed', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
+
+class ListingAdmin(admin.ModelAdmin):  # Ajoute une classe pour Listings (optionnel)
+    list_display = ('title', 'band')
+
+admin.site.register(Band,BandAdmin) # nous enregistrons le modèle Band avec l'admin de Django
+admin.site.register(Listings, ListingAdmin) # nous enregistrons le modèle Listings avec l'admin de Django
+
--- merchex/listing/models.py
+++ merchex/listing/models.py
@@ -1,9 +1,30 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here :
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2025)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+
+    def __str__(self):
+        return self.name
+    
+
+    
 class Listings(models.Model):
     title = models.fields.CharField(max_length=100)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
